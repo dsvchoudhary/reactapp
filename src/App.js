4114cba209f0697e720ddfdd46074de5398cf032
Deepak Choudhary <deepak.dhd@gmail.com>
2019-03-26 11:11:44 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      text: "my friend (from state)"
+      num1: 0,
+      num2: 0,
+      result: 0
     };
-    this.updateText =this.updateText.bind(this);
+    this.add = this.add.bind(this);
+    this.handlenum1Change = this.handlenum1Change.bind(this);
+    this.handlenum2Change = this.handlenum2Change.bind(this);
   }
-  updateText(){
-    this.setState(
-      {
-        text:"my friend (from changed state)"
-
-      }
-    );
-
+  handlenum1Change(event) {
+    this.setState({ num1: Number(event.target.value) });
+  }
+  handlenum2Change(event) {
+    this.setState({ num2: Number(event.target.value) });
+  }
+  add() {
+    this.setState({
+      result: this.state.num1 + this.state.num2
+    });
   }
   render() {
     return (
       <div className="App">
-      <h1>hi {this.state.text}</h1>
-<button onClick ={this.updateText}>click me!</button>
+        <input type="number" onChange={this.handlenum1Change} />
+        <input type="number" onChange={this.handlenum2Change} />
+        <button onClick={this.add}> Add</button>
+        <input type="text" value={this.state.result} />
       </div>
     );
   }
